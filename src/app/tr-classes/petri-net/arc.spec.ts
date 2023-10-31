b3d3bc63d9a3249e1648228ee3ba200cd7744ea5
Philipp Alber <philipp_j.alber@yahoo.com>
2023-10-31 20:15:16 +0100

    FP-10 remove test code

--- src/app/tr-classes/petri-net/arc.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Arc } from './arc';
-
-describe('Arc', () => {
-  it('should create an instance', () => {
-    expect(new Arc()).toBeTruthy();
-  });
-});
--- src/app/tr-classes/petri-net/transition.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Transition } from './transition';
-
-describe('Transition', () => {
-  it('should create an instance', () => {
-    expect(new Transition()).toBeTruthy();
-  });
-});
