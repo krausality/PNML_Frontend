5403c92d754a424a551cdb4c05b43399f0df4d14
marcel <marcel.naumann123@gmail.com>
2023-11-03 19:17:26 +0100

    FP-25: Fixed Typo

--- src/app/tr-services/data.service.ts
+++ src/app/tr-services/data.service.ts
@@ -31,7 +31,7 @@ export class DataService {
         return this._transitions
     }
 
-    getTranitionsAsync(): Observable<Transition[]> {
+    getTransitionsAsync(): Observable<Transition[]> {
         return of(this._transitions);
     }
 
