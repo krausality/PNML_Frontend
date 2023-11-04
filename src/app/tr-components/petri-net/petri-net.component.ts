a33cd6b6eab427f512f485be4bbb779da53a187c
Adrian Koder-Horsten <a.koder-horsten@sanalogic.com>
2023-11-04 20:00:34 +0100

    Second try for polylines with anchorspoints and white background

--- src/app/tr-components/petri-net/petri-net.component.ts
+++ src/app/tr-components/petri-net/petri-net.component.ts
@@ -25,7 +25,8 @@ export class PetriNetComponent {
     this.transitions = [
         new Transition(new Point(200, 150), "t1"),
         new Transition(new Point(600, 200), "t2"),
-        new Transition(new Point(1000, 250), "t3")
+        new Transition(new Point(1000, 250), "t3"),
+        new Transition(new Point(1600, 250), "t4")
     ];
 
     this.arcs = [
@@ -33,6 +34,8 @@ export class PetriNetComponent {
         new Arc(this.transitions[0], this.places[1], 1),
         new Arc(this.places[1], this.transitions[1], 1),
         new Arc(this.transitions[1], this.places[2], 1, [new Point(700, 300)]),
+        new Arc(this.transitions[2], this.places[3], 6, [new Point(800, 100), new Point(900, 350)]),
+        new Arc(this.places[3], this.transitions[3], 8, [new Point(1300, 100), new Point(1400, 300), new Point(1500, 100)]),
     ];
 
     this.transitions[0].appendPreArc(this.arcs[0]);
