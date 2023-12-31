6c6e495533965d6dd5174f7a6dcb6f72500144a1
mwietz <mwietz@hotmail.com>
2023-12-31 13:30:49 +0100

    FP-91: Display Long Labels
    Single Line Labels
    > - Width of labeled transitions is increased
    - Up to 10-character single line labels can be shown with the current settings
    - Background color for labeled transitions is changed to white for better readability
    
    Double Line Labels
    - A special character is used to indicate a line break within the label
    - The variable lineSeparator in position.constants.ts specifies the line separator
    - '~' is currently set as line separator

--- src/app/tr-services/position.constants.ts
+++ src/app/tr-services/position.constants.ts
@@ -4,7 +4,7 @@ export const placeIdYOffset = 45;
 
 // Transitions
 const transSilentWidthFactor = 0.80;
-export const transitionWidth = 2 * radius;
+export const transitionWidth = 4 * radius;
 export const transitionHeight = 2 * radius;
 export const transSilentWidth = transSilentWidthFactor * radius;
 export const transitionXOffset = transitionWidth / 2;
@@ -14,3 +14,6 @@ export const transitionIdYOffset = placeIdYOffset;
 
 // Anchors
 export const anchorRadius = 5;
+
+// Labels
+export const lineSeparator = '~';
