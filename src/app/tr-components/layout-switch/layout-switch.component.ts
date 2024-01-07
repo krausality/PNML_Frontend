826948f4e9b8fb023e0ca945996f5f25a9993496
Sonja Hartl <sonja-karl@web.de>
2024-01-07 12:38:37 +0100

    FP-86 updated layout switch to use two buttons instead of dropdown

--- src/app/tr-components/layout-switch/layout-switch.component.ts
+++ src/app/tr-components/layout-switch/layout-switch.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LayoutSpringEmbedderService } from 'src/app/tr-services/layout-spring-embedder.service';
 import { LayoutSugyiamaService } from 'src/app/tr-services/layout-sugyiama.service';
+import { showTooltipDelay } from 'src/app/tr-services/position.constants';
 
 @Component({
     selector: 'app-layout-switch',
@@ -8,22 +9,25 @@ import { LayoutSugyiamaService } from 'src/app/tr-services/layout-sugyiama.servi
     styleUrls: ['./layout-switch.component.css'],
 })
 export class LayoutSwitchComponent {
-    selected: string = 'free';
+    protected readonly showTooltipDelay = showTooltipDelay;
 
     constructor(
         private layoutSpringEmebdderService: LayoutSpringEmbedderService,
         private layoutSugyiamaService: LayoutSugyiamaService,
     ) {}
 
-    public layoutSwitchChanged() {
-        if (this.selected == 'spring-embedder') {
-            this.layoutSpringEmebdderService.layoutSpringEmbedder();
-        } else if (this.selected == 'sugiyama') {
-            // call sugiyama layout service
-            this.layoutSugyiamaService.applySugyiamaLayout();
-            this.layoutSpringEmebdderService.terminate();
-        } else {
-            this.layoutSpringEmebdderService.terminate();
+    public applyLayout(layoutAlgorithm: string) {
+        switch (layoutAlgorithm) {
+            case 'spring-embedder':
+                this.layoutSpringEmebdderService.layoutSpringEmbedder();
+                break;
+            case 'sugyiama':
+                this.layoutSpringEmebdderService.terminate();
+                this.layoutSugyiamaService.applySugyiamaLayout();
+                break;
+            default:
+                this.layoutSpringEmebdderService.terminate();
+                break;
         }
     }
 }
