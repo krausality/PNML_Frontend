1e4bc4ab0d17a320a7d402f4643a92d7b73b527c
Philipp Alber <philipp_j.alber@yahoo.com>
2024-01-08 18:20:40 +0100

    FP-88 fixed formatting

--- src/app/tr-components/code-editor/code-editor.component.ts
+++ src/app/tr-components/code-editor/code-editor.component.ts
@@ -7,23 +7,23 @@ import { PnmlService } from 'src/app/tr-services/pnml.service';
 import { UiService } from 'src/app/tr-services/ui.service';
 
 @Component({
-  selector: 'app-code-editor',
-  templateUrl: './code-editor.component.html',
-  styleUrls: ['./code-editor.component.css']
+    selector: 'app-code-editor',
+    templateUrl: './code-editor.component.html',
+    styleUrls: ['./code-editor.component.css'],
 })
 export class CodeEditorComponent {
-    languageSelected = "json";
+    languageSelected = 'json';
     textareaControl = new FormControl('');
 
     constructor(
         private exportJsonDataService: ExportJsonDataService,
-        private pnmlService: PnmlService
+        private pnmlService: PnmlService,
     ) {}
 
-    // loads the source code in json or pnml depending on 
+    // loads the source code in json or pnml depending on
     // which language is selected in the language switch
     public loadSourceCode() {
-        if (this.languageSelected === "json") {
+        if (this.languageSelected === 'json') {
             const jsonContent = this.exportJsonDataService.getJson();
             if (jsonContent) {
                 this.textareaControl.setValue(jsonContent);
