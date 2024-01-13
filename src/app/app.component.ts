7a0f59032a6b7ef6d192dbe39106ca0f5d6de874
hew84 <git@adriankoder.de>
2024-01-13 16:12:20 +0100

    FP-93 removed file content area and unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
@@ -7,14 +6,5 @@ import { FormControl } from '@angular/forms';
     styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-    public textareaFc: FormControl;
 
-    constructor() {
-        this.textareaFc = new FormControl();
-        this.textareaFc.disable();
-    }
-
-    public processSourceChange(newSource: string) {
-        this.textareaFc.setValue(newSource);
-    }
 }
