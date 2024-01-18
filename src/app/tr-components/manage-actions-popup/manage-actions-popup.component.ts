08983209d3410c105443134f88336a26375ffb1e
hew84 <git@adriankoder.de>
2024-01-18 21:13:20 +0100

    Revert "FP-96 Undo Added FormGroup"
    This reverts commit 49b7d7a41d1d91d031686ec4de16c41f37ca9c13.

--- src/app/tr-components/manage-actions-popup/manage-actions-popup.component.ts
+++ src/app/tr-components/manage-actions-popup/manage-actions-popup.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { DataService } from 'src/app/tr-services/data.service';
-import { FormBuilder } from '@angular/forms';
 
 @Component({
     selector: 'app-manage-actions-popup',
@@ -10,38 +9,21 @@ import { FormBuilder } from '@angular/forms';
 export class ManageActionsPopupComponent {
     actionSubmittedOnce = false;
 
-    checkoutForm = this.formBuilder.group({
-        line_1: '',
-        line_2: ''
-    });
-
-    constructor(
-        protected dataService: DataService,
-        private formBuilder: FormBuilder) {}
+    constructor(protected dataService: DataService) {}
 
     // check that the action is not already present and add it to the dataService
-    addAction(action: string) {
+    addAction(action: HTMLInputElement) {
         if (
-            !this.isActionEmpty(action) &&
-            !this.dataService.getActions().includes(action)
+            !this.isActionEmpty(action.value) &&
+            !this.dataService.getActions().includes(action.value)
         ) {
-            this.dataService.getActions().push(action);
+            this.dataService.getActions().push(action.value);
         }
-        action = '';
+        action.value = '';
         this.actionSubmittedOnce = true;
     }
 
     isActionEmpty(action: string): boolean {
         return !action.trim();
-    }
-
-    onSubmit(): void {
-        if((this.checkoutForm.value.line_1 && this.checkoutForm.value.line_2))
-        {
-
-            console.log(this.checkoutForm.value.line_1 + this.checkoutForm.value.line_2);
-            this.addAction(this.checkoutForm.value.line_1 + '~' + this.checkoutForm.value.line_2);
-        }
-        this.checkoutForm.reset();
     }
 }
