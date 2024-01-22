7a6bd66dee079001b54dcbbde001889f5c6b150f
hew84 <git@adriankoder.de>
2024-01-22 21:50:10 +0100

    FP-96 Added second Action Line.

--- src/app/tr-components/manage-actions-popup/manage-actions-popup.component.ts
+++ src/app/tr-components/manage-actions-popup/manage-actions-popup.component.ts
@@ -8,22 +8,47 @@ import { DataService } from 'src/app/tr-services/data.service';
 })
 export class ManageActionsPopupComponent {
     actionSubmittedOnce = false;
+    actionLine1: HTMLInputElement | undefined;
+    actionLine2: HTMLInputElement | undefined;
 
     constructor(protected dataService: DataService) {}
 
     // check that the action is not already present and add it to the dataService
-    addAction(action: HTMLInputElement) {
+    addAction(actionLine1: HTMLInputElement, actionLine2: HTMLInputElement) {
         if (
-            !this.isActionEmpty(action.value) &&
-            !this.dataService.getActions().includes(action.value)
+            !this.isActionEmpty(actionLine1.value) &&
+            this.isActionEmpty(actionLine2.value) &&
+            !this.dataService.getActions().includes(actionLine1.value)
         ) {
-            this.dataService.getActions().push(action.value);
+            this.dataService.getActions().push(actionLine1.value);
+        } else if (
+            this.isActionEmpty(actionLine1.value) &&
+            !this.isActionEmpty(actionLine2.value) &&
+            !this.dataService.getActions().includes(actionLine2.value)
+        ) {
+            this.dataService.getActions().push(actionLine2.value);
+        } else if (
+            !this.isActionEmpty(actionLine1.value) &&
+            !this.isActionEmpty(actionLine2.value) &&
+            !this.dataService.getActions().includes(actionLine1.value + '~' +actionLine2.value)
+        ) {
+            this.dataService.getActions().push(actionLine1.value + '~' +actionLine2.value);
         }
-        action.value = '';
+
+        actionLine1.value = '';
+        actionLine2.value = '';
         this.actionSubmittedOnce = true;
     }
 
     isActionEmpty(action: string): boolean {
         return !action.trim();
+    }
+
+    saveActionLine1(actionLine1: HTMLInputElement) {
+        this.actionLine1 = actionLine1;
+    }
+
+    saveActionLine2(actionLine2: HTMLInputElement) {
+        this.actionLine2 = actionLine2;
     }
 }
