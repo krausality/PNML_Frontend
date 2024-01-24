e43a57567de63c56102f1342e9341cb58a42436b
hew84 <git@adriankoder.de>
2024-01-24 22:14:27 +0100

    FP-96 Set label with new line icon, Inputs as columns

--- src/app/tr-components/manage-actions-popup/manage-actions-popup.component.ts
+++ src/app/tr-components/manage-actions-popup/manage-actions-popup.component.ts
@@ -8,6 +8,8 @@ import { DataService } from 'src/app/tr-services/data.service';
 })
 export class ManageActionsPopupComponent {
     actionSubmittedOnce = false;
+    bothActionsEmpty = false;
+    actionAlreadyExists = false;
     actionLine1: HTMLInputElement | undefined;
     actionLine2: HTMLInputElement | undefined;
 
@@ -20,24 +22,40 @@ export class ManageActionsPopupComponent {
             this.isActionEmpty(actionLine2.value) &&
             !this.dataService.getActions().includes(actionLine1.value)
         ) {
-            this.dataService.getActions().push(actionLine1.value);
+            this.dataService.getActions().push(actionLine1.value.trim());
+            this.bothActionsEmpty = false;
+            this.actionAlreadyExists = false;
         } else if (
             this.isActionEmpty(actionLine1.value) &&
             !this.isActionEmpty(actionLine2.value) &&
             !this.dataService.getActions().includes(actionLine2.value)
         ) {
             this.dataService.getActions().push(actionLine2.value);
+            this.bothActionsEmpty = false;
+            this.actionAlreadyExists = false;
         } else if (
             !this.isActionEmpty(actionLine1.value) &&
             !this.isActionEmpty(actionLine2.value) &&
             !this.dataService.getActions().includes(actionLine1.value + '~' +actionLine2.value)
         ) {
-            this.dataService.getActions().push(actionLine1.value + '~' +actionLine2.value);
+            this.dataService.getActions().push(actionLine1.value.trim() + '~' +actionLine2.value);
+            this.bothActionsEmpty = false;
+            this.actionAlreadyExists = false;
+        } else if (
+            this.isActionEmpty(actionLine1.value) &&
+            this.isActionEmpty(actionLine2.value)
+        ) {
+            this.bothActionsEmpty = true;
+        } else if (
+            this.dataService.getActions().includes(actionLine1.value) ||
+            this.dataService.getActions().includes(actionLine2.value) ||
+            this.dataService.getActions().includes(actionLine1.value + '~' +actionLine2.value)
+        ) {
+            this.actionAlreadyExists = true;
         }
 
         actionLine1.value = '';
         actionLine2.value = '';
-        this.actionSubmittedOnce = true;
     }
 
     isActionEmpty(action: string): boolean {
@@ -50,5 +68,17 @@ export class ManageActionsPopupComponent {
 
     saveActionLine2(actionLine2: HTMLInputElement) {
         this.actionLine2 = actionLine2;
+    }
+
+    returnLine1(action: string): string {
+        return (action.split("~"))[0];
+    }
+
+    returnLine2(action: string): string {
+        return (action.split("~"))[1];
+    }
+
+    containsBreakChar(action: string): boolean {
+        return action.includes('~');
     }
 }
